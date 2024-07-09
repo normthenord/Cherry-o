d26a9312a9c1dda35af65e208be416aded50665a
Stephen Burwell <xstompx@gmail.com>
2024-07-09 19:37:30 -0400

    Working right now!

---
+++ src/gameParts.rs
@@ -0,0 +1,85 @@
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
+use std::{collections::HashMap, i64::MAX};
+
+#[derive(Debug)]
+enum RollOption {
+    OneCherry,
+    TwoCherry,
+    ThreeCherry,
+    FourCherry,
+    Bird,
+    Dog,
+    OopsNoCherries,
+}
+
+impl Distribution<RollOption> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> RollOption {
+        // match rng.gen_range(0, 3) { // rand 0.5, 0.6, 0.7
+        match rng.gen_range(0..=7) {
+            // rand 0.8
+            0 => RollOption::OopsNoCherries,
+            1 => RollOption::OneCherry,
+            2 => RollOption::TwoCherry,
+            3 => RollOption::ThreeCherry,
+            4 => RollOption::FourCherry,
+            5 => RollOption::Bird,
+            _ => RollOption::Dog,
+        }
+    }
+}
+
+fn game() -> i64 {
+    let mut cherries: i64 = 10;
+    let mut count = 0;
+    loop {
+        let roll: RollOption = rand::random();
+        // println!("{:#?}", roll);
+        count = count + 1;
+        match roll {
+            RollOption::OneCherry => cherries = cherries - 1,
+            RollOption::TwoCherry => cherries = cherries - 2,
+            RollOption::ThreeCherry => cherries = cherries - 3,
+            RollOption::FourCherry => cherries = cherries - 4,
+            RollOption::Bird => cherries = cherries + 2,
+            RollOption::Dog => cherries = cherries + 2,
+            RollOption::OopsNoCherries => cherries = 10,
+        }
+
+        if cherries > 10 {
+            cherries = 10;
+        }
+        if cherries <= 0 {
+            break;
+        }
+    }
+    count
+}
+
+pub fn threaded_games(num: i64) -> (i64, i64, i64, HashMap<i64, i64>) {
+    let mut high_count = 0;
+    let mut low_count = MAX;
+    let mut total_count = 0;
+    let mut hash_counts = HashMap::new();
+
+    for _ in 1..num {
+        let game_count = game();
+        if hash_counts.contains_key(&game_count) {
+            hash_counts.insert(game_count, hash_counts.get(&game_count).unwrap() + 1);
+        } else {
+            hash_counts.insert(game_count, 1 as i64);
+        }
+        total_count = total_count + game_count;
+        if game_count > high_count {
+            high_count = game_count;
+        }
+        if game_count < low_count {
+            low_count = game_count;
+        }
+    }
+    // println!("{:?}", hash_counts);
+    (high_count, low_count, total_count, hash_counts)
+}
---
+++ src/utility.rs
@@ -0,0 +1,47 @@
+use std::{collections::HashMap, i64::MAX};
+
+pub fn games_above_threshold (threshold: i64, list:Vec<(&i64, &i64)> ) -> i64{
+    let mut count = 0;
+    for (key, value) in list.into_iter() {
+        if *key >= threshold {
+            count += value;
+        }
+    }
+    count
+
+}
+
+pub fn print_threshold(num_rolls: i64, big_hash_vec: Vec<(&i64, &i64)>, num_games: &i64){
+    let count = games_above_threshold(num_rolls, big_hash_vec.clone());
+    println!("Games with {num_rolls} rolls or more: {} ({:.2}% of the time)", count, count as f64/(*num_games) as f64 * 100.0 );
+}
+
+pub fn median_calc(num_games: &i64, list: Vec<(&i64, &i64)>) -> i64{
+    let mut c = 0;
+    let mut median: i64 = 0;
+    for (k, v) in list.into_iter() {
+        c += v;
+        if c > num_games / 2 {
+            median = *k;
+            break;
+        }
+    }
+    median
+}
+
+pub fn high_low_total_counts(hash_list:Vec<(i64, i64, i64, HashMap<i64, i64>)>) -> (i64, i64, i64){
+    let mut high_count = 0;
+    let mut low_count = MAX;
+    let mut total_count = 0;
+    for count in &hash_list {
+        if count.0 > high_count {
+            high_count = count.0;
+        }
+        if count.1 < low_count {
+            low_count = count.1
+        }
+        total_count = total_count + count.2;
+    }
+    (high_count, low_count, total_count)
+}
+
